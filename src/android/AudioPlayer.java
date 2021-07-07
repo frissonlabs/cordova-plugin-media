8fd20afa8a8569a742445af26b98f98383812f64
David Li <frissonlabs@gmail.com>
2021-07-06 18:17:58 -0700

    Fixed missing import

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -24,6 +24,7 @@ import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.media.MediaRecorder;
+import android.media.PlaybackParams;
 import android.os.Environment;
 
 import org.apache.cordova.LOG;
